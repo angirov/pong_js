bd783a35f6d38f263d8b45b890f1d4bd7a196034
Vladimir Angirov <angirov@posteo.net>
2023-11-16 13:51:59 +0100

    ball bounces sides

--- Ball.js
+++ Ball.js
@@ -45,11 +45,11 @@ export default class Ball {
     this.y += this.direction.y * this.velocity * delta;
 
     const rect = this.rect()
-    if (
-        rect.bottom >= window.innerHeight ||
-        rect.top <= 0
-    ) {
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
         this.direction.y *= -1
+    }
+    if (rect.right >= window.innerWidth || rect.left <= 0) {
+        this.direction.x *= -1
     }
     this.velocity += VELOCITY_INCREASE
     }
